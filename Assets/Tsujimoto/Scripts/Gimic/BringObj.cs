392802c3e54b83d7f0d44c64474da2dfea2ea668
kiri_main <hw23a073@oecu.jp>
2025-07-01 01:07:27 +0900

    20250701_魔法を箱で防げるように_Tsujimoto

--- Assets/Tsujimoto/Scripts/Gimic/BringObj.cs
+++ Assets/Tsujimoto/Scripts/Gimic/BringObj.cs
@@ -5,10 +5,24 @@ using UnityEngine;
 public class BringObj : MonoBehaviour
 {
     Vector3 startPos; //初期位置を格納する変数
+    Rigidbody rb;
+    Collider col;
 
     void Start()
     {
         startPos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        rb = GetComponent<Rigidbody>();
+        col = GetComponent<Collider>();
+    }
+
+    void Update()
+    {
+        //運ばれていないときはバグ回避のため、ここでも重力をオンにする
+        if (rb.velocity == Vector3.zero)
+        {
+            col.isTrigger = false;
+            rb.useGravity = true;
+        }
     }
 
     void OnTriggerEnter(Collider other)
@@ -16,6 +30,11 @@ public class BringObj : MonoBehaviour
         if (other.CompareTag("DeathArea"))
         {
             transform.position = startPos;
+        }
+
+        if (other.CompareTag("Majic"))
+        {
+            Destroy(other.gameObject);
         }
     }
 }
