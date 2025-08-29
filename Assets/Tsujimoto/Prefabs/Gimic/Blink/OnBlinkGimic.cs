e38beecebe6e156374609b52fb164236dd43b900
kiri_main <hw23a073@oecu.jp>
2025-08-30 01:46:42 +0900

    ステージ4の時間経過で消えるオブジェクト_Tsujimoto

---
+++ Assets/Tsujimoto/Prefabs/Gimic/Blink/OnBlinkGimic.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnBlinkGimic : MonoBehaviour
+{
+    [Header("同時に点滅させるオブジェクト")] [SerializeField] List<GameObject> blinkObj;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //プレイヤーが触れたら同時にオブジェクトを点滅開始
+        if (other.CompareTag("Player1") || other.CompareTag("Player2"))
+        {
+            foreach (var obj in blinkObj)
+            {
+                BlinkAndDestroy blinking = obj.GetComponent<BlinkAndDestroy>();
+                blinking.StartBlinking();    
+            }
+        }
+    }
+}
