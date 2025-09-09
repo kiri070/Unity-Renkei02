a446c5114fa546e7c58b67560fee4535b04944aa
kiri_main <hw23a073@oecu.jp>
2025-09-09 16:26:50 +0900

    ベルトコンベアの実装_Tsujimoto

---
+++ Assets/Tsujimoto/Scripts/Gimic/Beltconveyor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Beltconveyor : MonoBehaviour
+{
+
+    [Header("ベルトコンベアーの速度")]public float speed = 5f;
+    private void OnCollisionStay(Collision other)
+    {
+        if (other.gameObject.CompareTag("Player1") || other.gameObject.CompareTag("Player2") || other.gameObject.layer == LayerMask.NameToLayer("BringObj"))
+        {
+            Debug.Log(other.gameObject.name);
+            other.gameObject.GetComponent<Rigidbody>().AddForce(transform.forward * speed, ForceMode.Acceleration);
+        }
+    }
+}
