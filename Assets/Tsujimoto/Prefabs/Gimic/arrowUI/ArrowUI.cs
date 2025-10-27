c5c040ac604b4f4c8d224811d1819574b9341051
kiri_main <hw23a073@oecu.jp>
2025-10-27 20:38:55 +0900

    20251027_ギミックのチュートリアル_Tsujimoto

---
+++ Assets/Tsujimoto/Prefabs/Gimic/arrowUI/ArrowUI.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+/// <summary>
+/// 矢印を動かすスクリプト
+/// </summary>
+public class ArrowUI : MonoBehaviour
+{
+    void Start()
+    {
+        var startY = transform.localPosition.y; //y座標を取得
+        transform.DOLocalMoveY(startY + 1f, 2f).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutSine); //上下に動かす
+    }
+}
