812b91351495dc7337f41c99291ac0584560567f
kiri_main <hw23a073@oecu.jp>
2025-11-23 19:45:12 +0900

    20251123_バリアフェードアウト_Tsujimoto

---
+++ Assets/Tsujimoto/Scripts/Gimic/Fadeout_Barrier.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Fadeout_Barrier : MonoBehaviour
+{
+    [SerializeField] GameObject triggerObj;
+    bool isTrigger = false;
+    void Update()
+    {
+        if (!triggerObj.activeSelf && !isTrigger)
+        {
+            transform.DOScale(Vector3.zero, 1.5f).SetEase(Ease.InBack);
+            isTrigger = true;
+        }
+    }
+}
